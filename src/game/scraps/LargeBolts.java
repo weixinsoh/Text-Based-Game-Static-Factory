a0b0308c662df2af67d7bf586a39187fc8916652
weixin <wsoh0009@student.monash.edu>
2024-03-24 22:06:35 +1100

    Implemented LargeBolts and MetalSheets classes

---
+++ src/game/scraps/LargeBolts.java
@@ -0,0 +1,9 @@
+package game.scraps;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class LargeBolts extends Item {
+    public LargeBolts(String name, char displayChar, boolean portable) {
+        super(name, '+', portable);
+    }
+}
---
+++ src/game/scraps/MetalSheets.java
@@ -0,0 +1,10 @@
+package game.scraps;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class MetalSheets extends Item {
+    public MetalSheets(String name, boolean portable) {
+        super(name, '%', portable);
+    }
+
+}
