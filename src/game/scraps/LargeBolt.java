a99608bfb5bc352e7f829fe631a0e040181b5660
weixin <wsoh0009@student.monash.edu>
2024-04-05 20:24:23 +1100

    modified LargeBolt and MetalSheet to extends Item class and removed the restriction for Actor(only Intern) picking up these scraps

--- src/game/scraps/LargeBolt.java
+++ src/game/scraps/LargeBolt.java
@@ -2,7 +2,7 @@ package game.scraps;
 
 import edu.monash.fit2099.engine.items.Item;
 
-public class LargeBolt extends Scrap {
+public class LargeBolt extends Item {
     public LargeBolt() {
         super("large bolt", '+', true);
     }
---
+++ src/game/scraps/MetalSheet.java
@@ -0,0 +1,10 @@
+package game.scraps;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class MetalSheet extends Item {
+    public MetalSheet() {
+        super("metal sheet", '%', true);
+    }
+
+}
