3776eb808cb259addf4bc2a8e9ea6093dcf70767
weixin <wsoh0009@student.monash.edu>
2024-04-16 02:21:51 +1000

    change the name of the method getNextState() to getNextStage()

--- src/game/grounds/trees/Inheritree.java
+++ src/game/grounds/trees/Inheritree.java
@@ -3,11 +3,15 @@ package game.grounds.trees;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.specialscraps.fruits.Fruit;
+import game.scraps.fruits.Fruit;
 
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Abstract base class representing inheritree.
+ *
+ */
 public abstract class Inheritree extends Ground {
     public final Random random = new Random();
 
@@ -15,24 +19,48 @@ public abstract class Inheritree extends Ground {
 
     private int ticksBeforeGrow;
 
+    /**
+     * Constructor of the Inheritree class.
+     *
+     * @param displayChar the character that will represent the Inheritree in the display
+     * @param ticksBeforeGrow an integer representing the number of ticks needed to grow to next stage.
+     */
     public Inheritree(char displayChar, int ticksBeforeGrow) {
         super(displayChar);
         this.ticksBeforeGrow = ticksBeforeGrow;
     }
 
+    /**
+     * Grow a fruit.
+     *
+     * @return a new fruit.
+     */
     public abstract Fruit growFruit();
 
-    public Inheritree getNextState() {
+    /**
+     * Return the next stage of the inheritree to grow into.
+     *
+     * @return the next stage of the inheritree.
+     */
+    public Inheritree getNextStage() {
         return null;
     }
 
+    /**
+     * Drop fruit with a probability and grow to next stage after several ticks.
+     *
+     * Overrides Ground.tick(Location location)
+     *
+     * @see Ground#tick(Location)
+     * @param location The location of the Ground.
+     */
     @Override
     public void tick(Location location) {
         super.tick(location);
         count++;
 
-        if (count == ticksBeforeGrow + 1 && getNextState() != null) {
-            location.setGround(getNextState());
+        if (count == ticksBeforeGrow + 1 && getNextStage() != null) {
+            location.setGround(getNextStage());
         }
 
         List<Exit> exits = location.getExits();
@@ -44,6 +72,14 @@ public abstract class Inheritree extends Ground {
         }
     }
 
+    /**
+     * Set inheritree to block thrown objects.
+     *
+     * Overrides Ground.blocksThrownObjects()
+     *
+     * @see Ground#blocksThrownObjects()
+     * @return true indicating thrown objects will be blocked.
+     */
     @Override
     public boolean blocksThrownObjects() {
         return true;
